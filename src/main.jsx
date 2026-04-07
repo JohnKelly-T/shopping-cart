cdd02b119d37a0939b16103f02186306b60f8e45
john-kelly <johnkellytemena@gmail.com>
2026-04-07 17:29:39 +0800

    Import css reset file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import "../reset.css";
 import "./index.css";
 import Home from "./routes/home";
 import Root from "./routes/root";
