2ed90981d531b03e9101f8941d9ec3ceaca36058
john-kelly <johnkellytemena@gmail.com>
2026-05-11 17:45:43 +0800

    Move header element into a component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import { NavLink, Link, Outlet } from "react-router-dom";
+import { Handbag } from "lucide-react";
+import styles from "./Header.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+function Header() {
+  return (
+    <header className={cx("headerContainer")}>
+      <nav className={cx("navBar")}>
+        <ul>
+          <li>
+            <Link to="/" className={cx("siteName")}>
+              Faux.
+            </Link>
+          </li>
+          <li>
+            <NavLink to="/shop">Shop</NavLink>
+          </li>
+          <li>
+            <Link to="/cart" className={cx("shoppingCartLink")}>
+              <Handbag className={cx("shoppingCartIcon")} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
